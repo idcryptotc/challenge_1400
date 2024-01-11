e3da21a90fb7c377e4efbc13f286b02399569f91
fanateek <idcryptotc@yandex.ru>
2024-01-11 04:44:09 +0300

    Добавлено 22

--- Challenge/Challenge/chapter03.cpp
+++ Challenge/Challenge/chapter03.cpp
@@ -578,6 +578,22 @@ void chapter03::solution21()
 
 void chapter03::solution22()
 {
+    std::cout << "Введите трёхзначное число: ";
+    int n;
+    std::cin >> n;
+
+    if (checkInput())
+    {
+        if (n < 100 || n > 999)
+        {
+            std::cout << "Некорректный ввод\n";
+        }
+        else
+        {
+            std::string str = std::format("{1}{2}{0}\n", n / 100, n / 10 % 10, n % 10);
+            std::cout << "Результат: " << str;
+        }
+    }
 }
 
 void chapter03::solution23()
